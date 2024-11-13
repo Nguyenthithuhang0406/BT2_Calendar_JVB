7e9f89cc647f469c5356a93b774db111c6d4fb68
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-11-13 15:31:12 +0700

    update select month

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const now = document.getElementById("dateTime");
 const monthYear = document.getElementById("month");
-const dates = document.getElementById("calendar-date");
 
-const monthUp = document.getElementById("month-up");
-const monthDown = document.getElementById("month-down");
+const up = document.getElementById("month-up");
+const down = document.getElementById("month-down");
 const openMonthYear = document.getElementById("open-month-year");
 
 const selectedTime = document.getElementById("selected-time");
+const calendarContainer = document.getElementById("calendar-container");
 
 const today = new Date();
 
@@ -18,65 +18,77 @@ let selectedTimeDate = today.getDate();
 let selectedTimeMonth = today.getMonth();
 let selectedTimeYear = today.getFullYear();
 
-const renderCalendar = () => {
-  const monthName = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dayName = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+let view = "dateOfMonth";
 
+const monthName = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const dayName = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const renderCalendarHeader = () => {
   now.innerText = `${dayName[today.getDay()]}, ${
     monthName[today.getMonth()]
-  }, ${today.getDate()}`;
+  } ${today.getDate()}`;
+};
 
-  monthYear.innerText = `${
-    monthName[selectedMonth]
-  } ${selectedYear.toString()}`;
+const renderDateOfMonth = () => {
+  monthYear.innerText = `${monthName[selectedMonth]} ${selectedYear}`;
+  calendarContainer.innerHTML = "";
 
-  //ngay dau thang
-  const firstDay = new Date(selectedYear, selectedMonth);
-  //ngay cuoi thang
+  const calendarDay = document.createElement("div");
+  calendarDay.classList.add("calendar-day");
+  calendarDay.innerHTML = `
+      <div class="day">Su</div>
+      <div class="day">Mo</div>
+      <div class="day">Tu</div>
+      <div class="day">We</div>
+      <div class="day">Th</div>
+      <div class="day">Fr</div>
+      <div class="day">Sa</div>
+  `;
+  calendarContainer.appendChild(calendarDay);
+
+  const dates = document.createElement("div");
+  dates.classList.add("calendar-date");
+  calendarContainer.appendChild(dates);
+
+  const firstDay = new Date(selectedYear, selectedMonth, 1);
   const lastDay = new Date(selectedYear, selectedMonth + 1, 0);
-  //ngay cuoi thang truoc
   const lastDateOfLastMonth = new Date(selectedYear, selectedMonth, 0);
 
-  dates.innerHTML = "";
-
-  //hien thi cac ngay truoc thang hien tai
   for (let i = firstDay.getDay(); i > 0; i--) {
     const day = document.createElement("div");
-    day.classList.add("day");
+    day.classList.add("day", "inactive");
     day.innerText = lastDateOfLastMonth.getDate() - i + 1;
-    day.classList.add("inactive");
     dates.appendChild(day);
   }
 
-  //tạo các ngày trong tháng
   for (let i = 1; i <= lastDay.getDate(); i++) {
     const day = document.createElement("div");
     day.classList.add("day");
     day.innerText = i;
     dates.appendChild(day);
 
-    //danh dau today
     if (
       i === today.getDate() &&
       selectedMonth === today.getMonth() &&
@@ -85,41 +97,12 @@ const renderCalendar = () => {
       day.classList.add("today");
     }
 
-    //footer
-    selectedTime.innerText = "Today";
-  }
-
-  //neu ngay cuoi cung cua thang khong phai la chu nhat
-  //hien thi cac ngay sau thang hien tai
-  if (lastDay.getDay() !== 6) {
-    for (let i = 1; i <= 7 - lastDay.getDay(); i++) {
-      const day = document.createElement("div");
-      day.classList.add("day");
-      day.innerText = i;
-      day.classList.add("inactive");
-      dates.appendChild(day);
-    }
-  }
-
-  //neu ngay cuoi cung cua thang la chu nhat
-  //hien thi 1 tuan tiep theo
-  if (lastDay.getDay() === 6) {
-    for (let i = 1; i <= 7; i++) {
-      const day = document.createElement("div");
-      day.classList.add("day");
-      day.innerText = i;
-      day.classList.add("inactive");
-      dates.appendChild(day);
-    }
-  }
-
-  //chon ngay
-  const allDays = document.querySelectorAll(".day");
-  allDays.forEach((day) => {
     day.addEventListener("click", () => {
-      allDays.forEach((d) => d.classList.remove("selected"));
+      document
+        .querySelectorAll(".day")
+        .forEach((d) => d.classList.remove("selected"));
       day.classList.add("selected");
-      selectedTimeDate = day.innerText;
+      selectedTimeDate = i;
       selectedTimeMonth = selectedMonth;
       selectedTimeYear = selectedYear;
       selectedDay = new Date(
@@ -127,33 +110,70 @@ const renderCalendar = () => {
         selectedTimeMonth,
         selectedTimeDate
       ).getDay();
-
-      console.log();
-      //cap nhat tg o footer
       selectedTime.innerText = `${dayName[selectedDay]} ${selectedTimeDate}`;
     });
-  });
+  }
+
+  if (lastDay.getDay() !== 6) {
+    for (let i = 1; i <= 6 - lastDay.getDay(); i++) {
+      const day = document.createElement("div");
+      day.classList.add("day", "inactive");
+      day.innerText = i;
+      dates.appendChild(day);
+    }
+  }
 };
 
-// chuyen thang
-monthUp.addEventListener("click", () => {
-  if (selectedMonth === 11) {
-    selectedMonth = 0;
-    selectedYear++;
-  } else {
-    selectedMonth++;
+const changeMonth = (i) => {
+  if (view === "dateOfMonth") {
+    selectedMonth += i;
+    if (selectedMonth > 11) {
+      selectedMonth = 0;
+      selectedYear++;
+    } else if (selectedMonth < 0) {
+      selectedMonth = 11;
+      selectedYear--;
+    }
+    renderDateOfMonth();
+  } else if (view === "monthOfYear") {
+    selectedYear += i;
+    monthYear.innerText = `${selectedYear}`;
   }
-  renderCalendar();
+};
+
+const renderMonthOfYear = () => {
+  const monthOfYearContainer = document.createElement("div");
+  monthOfYearContainer.classList.add("monthOfYear");
+  calendarContainer.innerHTML = "";
+  calendarContainer.appendChild(monthOfYearContainer);
+
+  monthYear.innerText = `${selectedYear}`;
+
+  for (let i = 0; i < 12; i++) {
+    const monthElement = document.createElement("div");
+    monthElement.innerText = monthName[i].slice(0, 3);
+    monthElement.classList.add("monthElement");
+    monthOfYearContainer.appendChild(monthElement);
+
+    monthElement.addEventListener("click", () => {
+      selectedMonth = i;
+      view = "dateOfMonth";
+      renderDateOfMonth();
+    });
+  }
+};
+
+monthYear.addEventListener("click", () => {
+  view = view === "dateOfMonth" ? "monthOfYear" : "dateOfMonth";
+  view === "dateOfMonth" ? renderDateOfMonth() : renderMonthOfYear();
 });
 
-monthDown.addEventListener("click", () => {
-  if (selectedMonth === 0) {
-    selectedMonth = 11;
-    selectedYear--;
-  } else {
-    selectedMonth--;
-  }
-  renderCalendar();
-});
+up.addEventListener("click", () => changeMonth(1));
+down.addEventListener("click", () => changeMonth(-1));
 
-renderCalendar();
+const initializeCalendar = () => {
+  renderCalendarHeader();
+  renderDateOfMonth();
+};
+
+initializeCalendar();
